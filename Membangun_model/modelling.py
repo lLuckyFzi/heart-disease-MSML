03df9e87e5cf500ce5ae615613473f0ea5247656
lLuckyFzi <levryn04@gmail.com>
2025-12-24 05:54:09 +0700

    fix Kriteria 2 Basic for adding Autolog

--- Membangun_model/modelling.py
+++ Membangun_model/modelling.py
@@ -1,44 +1,27 @@
 import pandas as pd
 import mlflow
 import mlflow.sklearn
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+from sklearn.metrics import accuracy_score
 
 def train_basic():
     df = pd.read_csv("heart_disease_preprocessing/heart_disease_clean.csv")
     X = df.drop('target', axis=1)
     y = df['target']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42
-    )
+    mlflow.sklearn.autolog()
 
-    params = {"n_estimators": 100, "random_state": 42}
-    model = RandomForestClassifier(**params)
-    model.fit(X_train, y_train)
-
-    y_pred = model.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
-
-    mlflow.log_params(params)
-    mlflow.log_metric("accuracy", acc)
-    mlflow.sklearn.log_model(model, "model")
-
-    plt.figure(figsize=(6,5))
-    sns.heatmap(confusion_matrix(y_test, y_pred), annot=True, fmt='d', cmap='Blues')
-    plt.title("Confusion Matrix - Baseline")
-    plt.savefig("confusion_matrix.png")
-    mlflow.log_artifact("confusion_matrix.png")
-
-    report = classification_report(y_test, y_pred)
-    with open("classification_report.txt", "w") as f:
-        f.write(report)
-    mlflow.log_artifact("classification_report.txt")
-
-    print(f"Training Baseline selesai. Akurasi: {acc}")
+    with mlflow.start_run(run_name="Baseline_Autolog"):
+        params = {"n_estimators": 100, "random_state": 42}
+        model = RandomForestClassifier(**params)
+        model.fit(X_train, y_train)
+        
+        y_pred = model.predict(X_test)
+        acc = accuracy_score(y_test, y_pred)
+        
+        print(f"Training Baseline selesai. Akurasi: {acc}")
 
 if __name__ == "__main__":
     train_basic()
