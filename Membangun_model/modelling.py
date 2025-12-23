b2c63669036538e73addb3f62b468ffb28cdc45d
lLuckyFzi <levryn04@gmail.com>
2025-12-23 11:40:22 +0700

    feat modelling new v

--- Membangun_model/modelling.py
+++ Membangun_model/modelling.py
@@ -1,43 +1,44 @@
 import pandas as pd
 import mlflow
 import mlflow.sklearn
-import dagshub
-import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix, classification_report
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
 
+import dagshub
 dagshub.init(repo_owner='lLuckyFzi', repo_name='heart-disease-msml', mlflow=True)
 
-def train_model():
+def train_basic():
     df = pd.read_csv("heart_disease_preprocessing/heart_disease_clean.csv")
     X = df.drop('target', axis=1)
     y = df['target']
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    with mlflow.start_run(run_name="RandomForest_Baseline"):
-        params = {"n_estimators": 100, "random_state": 42}
-        model = RandomForestClassifier(**params)
+    with mlflow.start_run(run_name="Baseline_Model"):
+        model = RandomForestClassifier(random_state=42)
         model.fit(X_train, y_train)
-
+        
         y_pred = model.predict(X_test)
         acc = accuracy_score(y_test, y_pred)
-
-        mlflow.log_params(params)
+        
         mlflow.log_metric("accuracy", acc)
         mlflow.sklearn.log_model(model, "model")
-
-        plt.figure(figsize=(5,5))
-        sns.heatmap(confusion_matrix(y_test, y_pred), annot=True)
+        
+        plt.figure(figsize=(6,5))
+        sns.heatmap(confusion_matrix(y_test, y_pred), annot=True, fmt='d')
+        plt.title("Confusion Matrix")
         plt.savefig("confusion_matrix.png")
-        mlflow.log_artifact("confusion_matrix.png") 
+        mlflow.log_artifact("confusion_matrix.png")
+        
+        report = classification_report(y_test, y_pred)
+        with open("classification_report.txt", "w") as f:
+            f.write(report)
+        mlflow.log_artifact("classification_report.txt")
 
-        with open("requirements.txt", "w") as f:
-            f.write("pandas\nmlflow\ndagshub\nscikit-learn\nmatplotlib\nseaborn")
-        mlflow.log_artifact("requirements.txt")
-
-        print(f"Berhasil! Akurasi: {acc}. Cek Dashboard DagsHub Anda!")
+        print(f"Training selesai. Akurasi: {acc}")
 
 if __name__ == "__main__":
-    train_model()
+    train_basic()
